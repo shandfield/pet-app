a88e554378d160421cda97cd2bb23ab90884bd30
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-08-28 14:31:53 -0600

    heroku work

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("build"));
 }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:user1");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/book");
 
 // Start the API server
 app.listen(PORT, function() {
