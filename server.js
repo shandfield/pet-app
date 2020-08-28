da391040e556d301b51e6461df13e7c08f474ea5
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-08-28 14:25:44 -0600

    installed express and mongoose

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://secret-falls-79667.herokuapp.com/");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:user1");
 
 // Start the API server
 app.listen(PORT, function() {
