4e1942259d05ab6b7c6d0a7b0cd9fcea4f4d32bf
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-09-01 14:58:52 -0600

    got the seeds to show and the db to connect

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 //mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/animal");
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/animal";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reactanimallist";
   mongoose.connect(MONGODB_URI);
 
 // Start the API server
