606328d6a9c22fc3151453452c8132e9d0f524dd
Queen of Corgis <tana.owens@gmail.com>
2020-09-02 10:58:13 -0600

    working with Leo on UserPets page

---
+++ client/src/pages/UserPets.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PetCarousel from "../components/PetCarousel/PetCarousel";
+
+function UserPet() {
+    return (
+      <PetCarousel />
+    );
+  }
+  
+  export default UserPet;
+   
