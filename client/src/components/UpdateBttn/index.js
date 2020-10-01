a766cab76219f7b57f34cff562beabbc196f19fa
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-10-01 14:12:28 -0600

    made the code quiet so the button did not show on the page until it is ready to be used

---
+++ client/src/components/UpdateBttn/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+// function UpdateBtn(props) {
+//   return (
+//     <span className="update-btn" {...props} role="button" tabIndex="2">
+//       &#9998;
+//     </span>
+//   );
+// }
+
+// export default UpdateBtn;
